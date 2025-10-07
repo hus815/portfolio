24d93c3939535b8f66a8158780b685a76e03628e
hus815 <husnain.ali.77654@gmail.com>
2025-10-07 15:18:43 +0100

    Add files via upload

---
+++ hello cis1702.py
@@ -0,0 +1,10 @@
+("Hello, world!")
+student_name = "Husnain"
+student_age= 19
+print("student name:", student_age)
+print("student Age:", student_age)
+num1 = 15
+num2 = 10
+total = num1 + num2
+print("the sum is:", total)
+
